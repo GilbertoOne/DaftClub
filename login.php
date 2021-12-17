18b9a8fd9eb65c5ae6443c60d6872c8d428d4fdf
samuelmenad <96210971+samuelmenad@users.noreply.github.com>
2021-12-17 17:53:32 -0600

    cambio de login

--- login.php
+++ login.php
@@ -1,16 +1,26 @@
 <?php
-include('dbcon.php');
-$username = $_POST['username'];
-$password = $_POST['password'];
+if($_POST)//Recibe los datos del formulario del login
+{
+    include('dbcon.php');
+    $username = $_POST['username'];
+    $password = $_POST['password'];
 
-$query = $conn->query("select * from miembros where username = '$username' and password = '$password'");
-$count = $query->rowcount();
-$row = $query->fetch();
-if ($count > 0){
-session_start();
-$_SESSION['id'] = $row['id_miembro'];
- header('location:home.php'); 
-}else{
- header('location:index.php'); 
+    $query = $conn->query("select * from miembros where username = '$username' and password = '$password';");
+    //$count = $query->rowcount();
+    
+    $row = $query->fetch_assoc();
+    if ($query->num_rows > 0){
+    session_start();
+    $_SESSION['id'] = $row['id_miembro'];
+    header('Location: home.php');
+    
+    }else{
+    header('Location: index.php'); 
+    
+    }
+}
+else
+{
+    echo "Info no enviada";
 }
 ?>
