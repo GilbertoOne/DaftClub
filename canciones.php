40ad188dafbe8819885832477e2aa04b931140cb
DMayuscula <danife168@gmail.com>
2021-12-18 01:21:39 -0600

    youtube listo

--- canciones.php
+++ canciones.php
@@ -2,12 +2,18 @@
 <html>
   <body>
     <!-- 1. The <iframe> (and video player) will replace this <div> tag. -->
-    <div id="player"></div>
-
+    <?php 
+    $link[0] = "iOOK_sw9d7Y";
+    $link[1] = "zm9hb03dvaw";
+    $link[2] = "cpvC8_NMXmU";
+    for($i=0;$i<3;$i++){ ?>
+      <iframe id="player" type="text/html" width="640" height="360"
+  src="http://www.youtube.com/embed/<?php echo $link[$i] ?>?enablejsapi=1&origin=http://example.com" 
+  frameborder="0"></iframe>
+  <br>
     <script>
       // 2. This code loads the IFrame Player API code asynchronously.
       var tag = document.createElement('script');
-
       tag.src = "https://www.youtube.com/iframe_api";
       var firstScriptTag = document.getElementsByTagName('script')[0];
       firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
@@ -17,9 +23,6 @@
       var player;
       function onYouTubeIframeAPIReady() {
         player = new YT.Player('player', {
-          height: '360',
-          width: '640',
-          videoId: 'M7lc1UVf-VE',
           events: {
             'onReady': onPlayerReady,
             'onStateChange': onPlayerStateChange
@@ -45,6 +48,8 @@
       function stopVideo() {
         player.stopVideo();
       }
+    // Search for a specified string.
     </script>
+   <?php } ?>
   </body>
 </html>
